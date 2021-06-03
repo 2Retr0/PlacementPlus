cf0acf0e46cdfee100c002091c8131c94127adb5
2Retr0 <18603664+2Retr0@users.noreply.github.com>
2021-06-02 19:09:56 -0700

    Added ability to swap fences.

--- Patches/ReusablePatches.cs
+++ Patches/ReusablePatches.cs
@@ -2,13 +2,15 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 using Harmony;
+using Microsoft.Xna.Framework;
+using Netcode;
 using StardewModdingAPI;
 using StardewValley;
 using StardewValley.Buildings;
+using StardewValley.Network;
 using StardewValley.TerrainFeatures;
 using Object = StardewValley.Object;
 
@@ -21,44 +23,77 @@ namespace PlacementPlus.Patches
     {
         private static PlacementPlus.ModState modState => PlacementPlus.Instance.modState;
         private static IMonitor Monitor                => PlacementPlus.Instance.Monitor;
+        
+        public static readonly IEnumerable<Type> OBJECT_SUBCLASSES   = Assembly.Load("Stardew Valley").GetTypes()
+                                                                               .Where(t => t.IsSubclassOf(typeof(Object)));
 
-        [SuppressMessage("ReSharper", "ConvertToLocalFunction")]
+        public static readonly IEnumerable<Type> BUILDING_SUBCLASSES = Assembly.Load("Stardew Valley").GetTypes()
+                                                                               .Where(t => t.IsSubclassOf(typeof(Building)));
+
         private static IEnumerable<MethodBase> TargetMethods()
         {
-            // Get MethodInfo for each overwritten CheckForAction() in subclasses StardewValley.Object
-            Func<IEnumerable<MethodBase>> checkForActionIEnumerable = () => {
-                // * Begin checkForActionIEnumerable * //
-                return Assembly.Load("Stardew Valley").GetTypes()
-                    .Where(t => t.IsSubclassOf(typeof(Object)))
-                    .SelectMany(t => t.GetMethods())
-                    .Where(m => m.Name.Equals("checkForAction"));
-            };
+            // Get MethodInfo for each overwritten CheckForAction() in subclasses of StardewValley.Object
+            var checkForActionIEnumerable = OBJECT_SUBCLASSES.SelectMany(t => t.GetMethods())
+                                                             .Where(m => m.Name.Equals("checkForAction"));
             
-            // * Begin TargetMethods * //
-            // Concatenating other methods that we want to skip.
-            return checkForActionIEnumerable().Concat(new [] {
-                AccessTools.Method(typeof(GameLocation), nameof(GameLocation.performAction)),
-                AccessTools.Method(typeof(ShippingBin),  nameof(ShippingBin.leftClicked))
-            });
+            // Get MethodInfo for each overwritten doAction() in subclasses of StardewValley.Building
+            var doAcionIEnumerable        = BUILDING_SUBCLASSES.SelectMany(t => t.GetMethods())
+                                                             .Where(m => m.Name.Equals("doAction"));
+            
+            // Including other classes with StardewValley.Object subclasses.
+            return checkForActionIEnumerable.Concat(
+                   doAcionIEnumerable.Concat(new [] {
+                       AccessTools.Method(typeof(GameLocation), nameof(GameLocation.performAction)),
+                       AccessTools.Method(typeof(ShippingBin),  nameof(ShippingBin.leftClicked)),
+                       AccessTools.Method(typeof(Building),     nameof(Building.doAction))
+                   }));
         }
 
         
         
-        private static bool Prefix(ref bool __result)
+        private static bool Prefix(ref bool __result, MethodBase __originalMethod)
         {
             try 
             {
-                var tileAtCursorIsFlooring = modState.currentTerrainFeatures.ContainsKey(modState.tileAtPlayerCursor) && 
-                                             modState.currentTerrainFeatures[modState.tileAtPlayerCursor] is Flooring;
-                var isHoldingFlooring      = modState.currentlyHeldItem?.category.Value == Object.furnitureCategory;
+                Func<NetVector2Dictionary<TerrainFeature,NetRef<TerrainFeature>>, Vector2, bool> flooringTileChecks = (tf, t) => {
+                    // We need to use Game1.didPlayerJustLeftClick() since modState.isHoldingUseToolButton does not
+                    // cover the initial button press.
+                    var preliminaryChecks = modState.currentlyHeldItem != null                      &&
+                                            // If the method's class subclasses Object or Building.
+                                            (__originalMethod.DeclaringType == typeof(ShippingBin)  && 
+                                             __originalMethod.DeclaringType == typeof(GameLocation) ||
+                                             // If the Player is holding left click.
+                                             Game1.didPlayerJustLeftClick() || modState.isHoldingUseToolButton);
+                    
+                    // * Begin flooring tile checks * //
+                    if (!preliminaryChecks) return false; // Preliminary checks.
+
+                    var tileAtCursorIsFlooring = tf.ContainsKey(t) && tf[t] is Flooring;
+                    var isHoldingFlooring      = modState.currentlyHeldItem.category.Value == Object.furnitureCategory;
+
+                    // If the player-held item is the flooring they are looking at OR the player holding flooring.
+                    return tileAtCursorIsFlooring && !PlacementPlus.Instance.FlooringAtTileIsPlayerItem(modState.currentlyHeldItem, t) || 
+                           isHoldingFlooring;
+                };
                 
-                // Run original logic if the player is not holding flooring or the player-held item is the flooring they are looking at.
-                if (!isHoldingFlooring     ||
-                    tileAtCursorIsFlooring && 
-                    PlacementPlus.Instance.FlooringAtTileIsPlayerItem(
-                         modState.currentlyHeldItem, 
-                         modState.tileAtPlayerCursor)
-                ) return true;
+                Func<GameLocation, Vector2, bool> fenceTileChecks = (gl, t) => {
+                    // * Begin fence tile checks * //
+                    var objectAtTile   = gl.getObjectAtTile((int) t.X, (int) t.Y);
+                    var isHoldingFence = modState.currentlyHeldItem is Fence;
+                    
+                    // If the fence the player is looking at is a gate AND the player-held item is a fence.
+                    return __originalMethod.DeclaringType == typeof(Fence) &&
+                           objectAtTile is Fence fence && fence.isGate     && 
+                           isHoldingFence;
+                };
+
+                // * Begin Prefix * //
+                if (
+                    !fenceTileChecks(modState.currentPlayer.currentLocation, modState.tileAtPlayerCursor) &&
+                    !flooringTileChecks(modState.currentTerrainFeatures, modState.tileAtPlayerCursor)
+                ) {
+                    return true;
+                }
 
                 __result = false; // Original method will now return false.
                 return false;     // Skip original logic.
