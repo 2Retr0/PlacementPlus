bf4aff5b8faa69f518e7a256c9a009d4bf8bc13f
2Retr0 <18603664+2Retr0@users.noreply.github.com>
2022-08-25 17:51:05 -0700

    Create BiDictionary.cs

---
+++ Utility/BiDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace PlacementPlus.Utility
+{
+    /// <summary> Helper data structure representing a bi-directional mapping between two of the same type. </summary>
+    // Adapted from Dave Zych: https://stackoverflow.com/a/32658403
+    public class BiDictionary<T> : IEnumerable
+    {
+        private readonly Dictionary<T, T> leftDict = new(), rightDict = new();
+
+        /// <summary> Adds the specified key and value to the dictionary. </summary>
+        public void Add(T leftItem, T rightItem)
+        {
+            leftDict.Add(leftItem, rightItem); 
+            rightDict.Add(rightItem, leftItem);
+        }
+
+        
+        
+        /// <summary> Determines whether the BiDictionary contains the specified key. </summary>
+        public bool ContainsKey(T item) => leftDict.ContainsKey(item) || rightDict.ContainsKey(item);
+        
+        
+        
+        /// <summary> Gets the value associated with the specified key. </summary>
+        public T this[T value]
+        {
+            get
+            {
+                if (leftDict.ContainsKey(value)) return leftDict[value];
+                if (rightDict.ContainsKey(value)) return rightDict[value];
+                    
+                throw new KeyNotFoundException(null, null);
+            }
+        }
+        
+        IEnumerator IEnumerable.GetEnumerator() => leftDict.GetEnumerator();
+    }
+}
